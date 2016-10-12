2af35a82d2fa98208f0bfcde7bf0d3d4fb4c0278
Josh Langholtz <jjlangholtz@gmail.com>
2016-10-12 15:56:51 -0400

    Add specs for github api

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -11,6 +11,9 @@ end
 
 require 'spec_helper'
 require 'rspec/rails'
+require 'webmock/rspec'
+WebMock.disable_net_connect!
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
